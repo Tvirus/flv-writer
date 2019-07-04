07d066a78fc0862c1c61e7b4e2f0de3df60fa597
L <42616862+Tvirus@users.noreply.github.com>
2019-07-04 18:33:41 +0800

    Update FLVWriter.h

--- FLVWriter.h
+++ FLVWriter.h
@@ -2,32 +2,18 @@
 #define _FLVWRITER_H_
 
 
-#ifndef u8
-    #define u8   unsigned char
-#endif
-#ifndef s8
-    #define s8     signed char
-#endif
-#ifndef u16
-    #define u16  unsigned short
-#endif
-#ifndef s16
-    #define s16    signed short
-#endif
-#ifndef u32
-    #define u32  unsigned int
-#endif
-#ifndef s32
-    #define s32    signed int
-#endif
-#ifndef u64
-    #define u64  unsigned long long
-#endif
-#ifndef s64
-    #define s64    signed long long
-#endif
+typedef    unsigned char          u8;
+typedef      signed char          s8;
+typedef    unsigned short        u16;
+typedef      signed short        s16;
+typedef    unsigned int          u32;
+typedef      signed int          s32;
+typedef    unsigned long long    u64;
+typedef      signed long long    s64;
 
 
+
+/* MPEG-4 Audio Object Types */
 /* http://wiki.multimedia.cx/index.php?title=MPEG-4_Audio */
 #define MP4_AUDIO_TYPE_INVALID      0
 #define MP4_AUDIO_TYPE_AAC_MAIN     1
@@ -52,8 +38,8 @@ typedef struct
     //u8  SampleSize; /* 16 or 8 */
     u16 SampleRate; /* Hz */
     u8  Stereo;     /* 0:mono  1:stereo */
-    u8  AudioType;  /* aac needed, MPEG-4 Audio Object Types */
-    u8  Channel;
+    u8  AudioType;  /* aac 格式的具体类型, MPEG-4 Audio Object Types */
+    u8  Channel;    /* 通道个数 */
 
 }T_FLVConfig;
 
